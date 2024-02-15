91ed733c5276a543f2abc4a1d26be57714b32b24
Joe Bottigliero <694253+jbottigliero@users.noreply.github.com>
2024-02-15 15:10:40 -0600

    chore: initial commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# static-data-portal
+
+**🧪 EXPERIMENTAL 🧪**
+
+This is a working proof-of-concept of a Globus-powered Search portal `generator` created using Next.js.
+
+---
+
+This is a [Next.js](https://nextjs.org/) project bootstrapped with [`create-next-app`](https://github.com/vercel/next.js/tree/canary/packages/create-next-app).
+
---
+++ pages/index.tsx
@@ -0,0 +1,17 @@
+import Head from "next/head";
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Globus : Static Data Portal</title>
+        <meta name="description" content="A Globus-powered research data portal." />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <main>
+        
+      </main>
+    </>
+  );
+}
