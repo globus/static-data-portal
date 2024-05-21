0b61528f6d604daecbb526517054a541f99207a9
Joe Bottigliero <694253+jbottigliero@users.noreply.github.com>
2024-05-21 08:47:06 -0500

    refactor: adds isDirectory utility, cleans up file entry rendering

--- components/file-browser/FileEntryIcon.tsx
+++ components/file-browser/FileEntryIcon.tsx
@@ -1,11 +1,12 @@
 import React from "react";
 import { Icon } from "@chakra-ui/react";
 import { FolderIcon, DocumentIcon } from "@heroicons/react/24/outline";
+import { isDirectory } from "@/utils/globus";
 
 import type { FileDocument } from "@globus/sdk/cjs/lib/services/transfer/service/file-operations";
 
 export default function FileEntryIcon({ entry }: { entry: FileDocument }) {
-  if (entry.type === "dir") {
+  if (isDirectory(entry)) {
     return <Icon as={FolderIcon} />;
   }
   return <Icon as={DocumentIcon} />;
