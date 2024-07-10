861f53c4a5e5b75b7dfbbedbe7dc3f7c571a98a5
Joe Bottigliero <694253+jbottigliero@users.noreply.github.com>
2024-07-10 15:54:14 -0500

    feat: adds support for MDX

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,16 @@
 import STATIC from "./static.json" assert { type: "json" };
 
+import mdx from "@next/mdx";
+
+const withMDX = mdx({
+  extension: /\.(md|mdx)$/,
+});
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "export",
   reactStrictMode: true,
+  pageExtensions: ["js", "jsx", "md", "mdx", "ts", "tsx"],
   /**
    * If the `static.json` file contains a `host` object, use the `base_path` value
    * as the `basePath` for the Next.js application.
@@ -11,4 +18,4 @@ const nextConfig = {
   basePath: STATIC._static?.host?.base_path || undefined,
 };
 
-export default nextConfig;
+export default withMDX(nextConfig);
