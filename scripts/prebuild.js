2e5945dd220a29782c1e860a7f796be174c73873
Joe Bottigliero <694253+jbottigliero@users.noreply.github.com>
2024-08-16 16:08:01 -0500

    feat: support content/assets/* => public/*

--- scripts/prebuild.js
+++ scripts/prebuild.js
@@ -2,21 +2,35 @@
  * Node.js script to be run before `npm run build`
  */
 
-const { stat, cp } = require("node:fs/promises");
+const { stat, cp, rm } = require("node:fs/promises");
 const { join } = require("node:path");
 
 /**
- * Sync the user-provided `content` directory with the `pages` directory.
+ * Sync the user-provided `content` directory with the `pages` and `public` directory.
  */
 async function syncContentDirectory() {
   const CONTENT_DIR = join(__dirname, "..", "content");
-  const exists = await stat(CONTENT_DIR).catch(() => false);
-  if (!exists || !exists.isDirectory()) {
+  const contentDirStat = await stat(CONTENT_DIR).catch(() => false);
+  if (!contentDirStat || !contentDirStat.isDirectory()) {
     return;
   }
   /**
-   * Copy the contents of the directory to the `pages` directory.
+   * If there is an `assets` directory, we'll copy its contents to the `public` directory
+   * before copying the rest of the `content` directory to `pages`.
    */
+  const ASSETS_DIR = join(CONTENT_DIR, "assets");
+  const assetsDirStat = await stat(ASSETS_DIR).catch(() => false);
+
+  if (assetsDirStat && assetsDirStat.isDirectory()) {
+    await cp(ASSETS_DIR, join(__dirname, "..", "public"), {
+      recursive: true,
+      force: true,
+    });
+    /**
+     * Remove the `assets` directory from the `content` directory (workspace).
+     */
+    await rm(ASSETS_DIR, { recursive: true });
+  }
   await cp(CONTENT_DIR, join(__dirname, "..", "pages"), {
     recursive: true,
     force: true,
