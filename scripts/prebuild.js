e3754a9ad0e22daa4c4b6e676782f39ee12b1704
Joe Bottigliero <694253+jbottigliero@users.noreply.github.com>
2024-08-19 11:08:38 -0500

    fix: `content` file overrides for well-known pages (e.g. `index.*`, `privacy-policy.*`), no longer need to match the generator's extension (#224)
    This will simplify the ability to overwrite these files and avoid
    duplicate page files when a user defines a `index.md` file, but we have
    a `index.mdx` file.

--- scripts/prebuild.js
+++ scripts/prebuild.js
@@ -4,6 +4,59 @@
 
 const { stat, cp, rm } = require("node:fs/promises");
 const { join } = require("node:path");
+const { glob } = require("glob");
+
+/**
+ * `OVERRIDES` is an array of files that we want to ensure are overriden by the user's
+ * custom files regardless of the extension they provide.
+ *
+ * For example, if a users provides a `index.md` file for their landing page, we want to
+ * make sure that takes precedence over the default `index.mdx` file in the generator.
+ */
+const OVERRIDES = [
+  {
+    file: "index.mdx",
+    pattern: "index.@(js|ts|md)?(x)",
+  },
+  {
+    file: "privacy-policy.mdx",
+    pattern: "privacy-policy.@(js|ts|md)?(x)",
+  },
+  {
+    file: "terms-and-conditions.mdx",
+    pattern: "terms-and-conditions.@(js|ts|md)?(x)",
+  },
+];
+
+/**
+ * Process all of the configured overrides.
+ */
+function handleOverrides() {
+  return Promise.all(
+    OVERRIDES.map(async ({ file, pattern }) => {
+      /**
+       * Check to see if the user has provided a file that matches the pattern.
+       */
+      const hasCustomFile = await glob(pattern, {
+        cwd: join(__dirname, "..", "content"),
+      }).then((files) => files.length > 0);
+      if (hasCustomFile) {
+        const defaultFile = join(__dirname, "..", "pages", file);
+        /**
+         * Check to see if the default (configured) file exists.
+         * This ensures commands like `preview` don't need "pristine" working directories to function.
+         */
+        if (await stat(defaultFile).catch(() => false)) {
+          /**
+           * If so, remove the default (configured) file from the `pages` directory.
+           */
+
+          await rm(join(__dirname, "..", "pages", file));
+        }
+      }
+    }),
+  );
+}
 
 /**
  * Sync the user-provided `content` directory with the `pages` and `public` directory.
@@ -31,6 +84,9 @@ async function syncContentDirectory() {
      */
     await rm(ASSETS_DIR, { recursive: true });
   }
+
+  await handleOverrides();
+
   await cp(CONTENT_DIR, join(__dirname, "..", "pages"), {
     recursive: true,
     force: true,
