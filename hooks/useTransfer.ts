b52f47508cf1a49eb600ddbbcff74c6c62c868da
Joe Bottigliero <694253+jbottigliero@users.noreply.github.com>
2025-03-18 18:30:09 -0500

    fix: clean-up useTransfer hooks

--- hooks/useTransfer.ts
+++ hooks/useTransfer.ts
@@ -5,38 +5,45 @@ import { useGlobusAuth } from "@globus/react-auth-context";
 import { type AuthorizationManager } from "@globus/sdk/core/authorization/AuthorizationManager";
 
 /**
- * Used to wrap `Response` objects returned from the @globus/sdk to
- * ensure HTTP encountered errors throw a rejected promise.
+ * Used to wrap @globus/sdk methods to ensure HTTP encountered errors throw a rejected promise.
  * @see https://tanstack.com/query/latest/docs/framework/react/guides/query-functions#usage-with-fetch-and-other-clients-that-do-not-throw-by-default
  */
-async function handleResponse(response: Response) {
+async function dispatch<R extends Response>(
+  request: Promise<R>,
+): Promise<Awaited<ReturnType<R["json"]>>> {
+  const response = await request;
   if (response.ok) {
     return await response.json();
   }
-  return Promise.reject(await response.json());
+  throw await response.json();
 }
 
-export async function fetchCollection(
+async function fetchCollection(
   authorization: AuthorizationManager | undefined,
   id: string,
 ) {
-  return handleResponse(
-    await transfer.endpoint.get(id, {}, { manager: authorization }),
+  return await dispatch(
+    transfer.endpoint.get(id, {}, { manager: authorization }),
   );
 }
 
 /**
  * @see https://docs.globus.org/api/transfer/endpoints_and_collections/#endpoint_or_collection_document
  */
-export type Collection = Awaited<ReturnType<typeof fetchCollection>>;
+export type Collection = Awaited<
+  ReturnType<Awaited<ReturnType<typeof transfer.endpoint.get>>["json"]>
+>;
 
-export function useCollection(id: string, placeholderData?: Collection) {
+export function useCollection(
+  id: string,
+  placeholderData?: Partial<Collection>,
+) {
   const auth = useGlobusAuth();
   return useQuery({
     enabled: auth.isAuthenticated,
     queryKey: ["collections", id],
-    queryFn: () => fetchCollection(auth.authorization, id),
-    placeholderData,
+    queryFn: async () => await fetchCollection(auth.authorization, id),
+    // placeholderData,
   });
 }
 
@@ -46,8 +53,8 @@ export function useEndpointSearch(query = {}) {
     enabled: auth.isAuthenticated,
     queryKey: ["endpoints", "search", JSON.stringify(query)],
     queryFn: async () => {
-      return handleResponse(
-        await transfer.endpointSearch(
+      return await dispatch(
+        transfer.endpointSearch(
           {
             query,
           },
@@ -66,18 +73,11 @@ async function ls(
   path?: string,
   options: Parameters<typeof transfer.fileOperations.ls>[1] = {},
 ) {
-  return handleResponse(
-    await transfer.fileOperations.ls(
+  return await dispatch(
+    transfer.fileOperations.ls(
       id,
-      {
-        query: {
-          path: path ?? undefined,
-          ...(options?.query || {}),
-        },
-      },
-      {
-        manager: authorization,
-      },
+      { query: { path, ...options.query } },
+      { manager: authorization },
     ),
   );
 }
@@ -91,7 +91,7 @@ export function useListDirectory(
   return useQuery({
     enabled: auth.isAuthenticated,
     queryKey: ["collections", id, "ls", path, options],
-    queryFn: () => ls(auth.authorization, id, path, options),
+    queryFn: async () => await ls(auth.authorization, id, path, options),
   });
 }
 
@@ -101,8 +101,8 @@ export function useCollectionBookmarks() {
     enabled: auth.isAuthenticated,
     queryKey: ["bookmarks"],
     queryFn: async () => {
-      return handleResponse(
-        await transfer.collectionBookmarks.getAll(
+      return await dispatch(
+        transfer.collectionBookmarks.getAll(
           {},
           {
             manager: auth.authorization,
